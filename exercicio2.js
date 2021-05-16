bdd3814a9cf37bef08811a559ba2cd4b617ab447
Amanda Santos <amandasantosvip@hotmail.com>
2021-05-16 19:42:23 -0300

    Adicionando exercicio 2

---
+++ exercicio2.js
@@ -0,0 +1,10 @@
+//2 - Calcule o tempo de uma viagem de carro. Pergunte a distância a percorrer e a velocidade média esperada para a viagem.
+
+let distanciaPercorrer = 80 // aqui obtemos a distancia a ser percorrida
+let velocidadeMedia = 100 // aqui obtemos a velocidade média
+let tempo // variavel criada para receber o valor referente ao tempo que levará para chegar ao destino
+
+tempo = velocidadeMedia / distanciaPercorrer // calculo do tempo até chegar ao destino
+
+
+console.log(tempo) // imprimindo resposta na tela

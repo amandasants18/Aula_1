e4c08492cdf26e6f1deb49aa752a85a8b9e0fc1b
Amanda Santos <amandasantosvip@hotmail.com>
2021-05-16 22:11:24 -0300

    Adicionando exercicio 6

---
+++ exercicio6.js
@@ -0,0 +1,29 @@
+let entrada = 4
+
+if (entrada === 1) {
+
+    console.log("Hoje é domingo")
+
+} else if (entrada === 2){
+
+    console.log("Hoje é segunda")
+}else if(entrada === 3){
+
+    console.log("Hoje é terça")
+}else if(entrada === 4){
+
+    console.log("Hoje é quarta")
+}else if(entrada === 5){
+
+    console.log("Hoje é quinta")
+}else if(entrada === 6){
+
+
+    console.log("Hoje é sexta")
+}else if(entrada === 7){
+
+    console.log("Hoje é sábado")
+}else{
+
+    console.log("Informe um dia válido")
+}

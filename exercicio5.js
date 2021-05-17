5e9ca05aa179acd3e08ec83fb085375a76ae3652
Amanda Santos <amandasantosvip@hotmail.com>
2021-05-16 22:02:06 -0300

    Adicionando exercicio 5

---
+++ exercicio5.js
@@ -0,0 +1,10 @@
+//5 - Receba do usuários 3 números e calcule a média entre eles.
+
+let num1 = 5 // variavel com o primeiro numero
+let num2 = 10 // variavel com o segundo numero
+let num3 = 3 // variavel com o terceiro numero
+let media // variavel para calcular a média
+
+media = (num1 + num2 + num3) / 3 // somando todos os números e dividindo pela quantidade
+
+console.log(media) // imprimindo o resultado na tela

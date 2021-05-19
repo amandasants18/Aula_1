bf1ac94402e1120d0cd4c9316a6faf31ac6c14c5
Amanda Santos <amandasantosvip@hotmail.com>
2021-05-19 17:53:51 -0300

    Adicionando exercicio 9

---
+++ exercicio9.js
@@ -0,0 +1,11 @@
+// 9 - Crie uma função que retorna a palavra (impar/par) de acordo com seu parâmetro.
+
+let num1 = 7 // atribuindo o numero 7 a variavel num1
+
+if (num1 % 2 === 0){    // se o num1 for divisivel por 2, entao o número é par
+
+    console.log("O número é par")
+}else {                              // senao, o número é ímpar
+
+    console.log("O número é ímpar")
+}

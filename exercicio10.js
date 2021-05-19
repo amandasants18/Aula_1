b44a34c2ac0a05a79606b1c9f6e8f444984316b4
Amanda Santos <amandasantosvip@hotmail.com>
2021-05-19 18:00:05 -0300

    Adicionando exercicio 10

---
+++ exercicio10.js
@@ -0,0 +1,17 @@
+//10 - Crie uma função que recebe o ano de nascimento da pessoa informando se ela é maior de idade ou menor.
+
+
+let ano = 1995; // atribuindo um ano a variavel ano
+let idade // criei uma variavel idade, para guardar a idade
+
+idade = 2021 - ano // calculo para encontrar a idade
+
+if (idade < 18){   // se idade for menor que 18, é menor de idade
+
+    console.log("Você é menor de idade")
+}else{
+
+    console.log("Você é maior de idade") // senao, é maior de idade
+}
+
+
